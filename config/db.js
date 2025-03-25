91a9121207ac299f24c0c38b45299ca673890925
zhaoming12345 <zhaoming19900101@163.com>
2025-03-25 15:28:15 +0800

    Create db.js
    创建 db.js 文件

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'admin_system',
+    insecureAuth: true
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('数据库连接失败：', err);
+        return;
+    }
+    console.log('数据库连接成功');
+});
+
+module.exports = connection; 
